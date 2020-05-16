a452b4e0a087d7c3075bce18e94730965f6b16b2
Pankaj <pankajsbagal@gmail.com>
2020-05-16 11:12:41 +0530

    created new project and added splash screen

---
+++ app/src/main/java/com/dev/pankaj/gcreationv1/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.dev.pankaj.gcreationv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_SCREEN_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+    }
+}
