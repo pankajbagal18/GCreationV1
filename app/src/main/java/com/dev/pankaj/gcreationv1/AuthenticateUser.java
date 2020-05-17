cf01198c02544dbc13b7b7f49f19ef284eb457a9
Pankaj <pankajsbagal@gmail.com>
2020-05-17 12:27:36 +0530

    added firebase phone auth

---
+++ app/src/main/java/com/dev/pankaj/gcreationv1/AuthenticateUser.java
@@ -0,0 +1,132 @@
+package com.dev.pankaj.gcreationv1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class AuthenticateUser extends AppCompatActivity {
+
+    EditText editTextPhone, editTextCode;
+
+    FirebaseAuth mAuth;
+
+    String codeSent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate_user);
+        ContextCompat.checkSelfPermission(this, Manifest.permission_group.SMS);
+        mAuth = FirebaseAuth.getInstance();
+        editTextCode = findViewById(R.id.editTextCode);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+        findViewById(R.id.buttonGetVerificationCode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendVerificationCode();
+            }
+        });
+
+
+        findViewById(R.id.buttonSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifySignInCode();
+            }
+        });
+    }
+    private void verifySignInCode(){
+        String code = editTextCode.getText().toString();
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //here you can open new activity
+                            Toast.makeText(getApplicationContext(),
+                                    "Login Successfull", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(AuthenticateUser.this,MainActivity.class));
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Incorrect Verification Code ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(){
+
+        String phone = editTextPhone.getText().toString();
+
+        if(phone.isEmpty()){
+            editTextPhone.setError("Phone number is required");
+            editTextPhone.requestFocus();
+            return;
+        }
+
+        if(phone.length() < 10 ){
+            editTextPhone.setError("Please enter a valid phone");
+            editTextPhone.requestFocus();
+            return;
+        }
+
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91"+phone,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            Log.d("verification","successful");
+            signInWithPhoneAuthCredential(phoneAuthCredential);
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Log.d("verification","un successful");
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            //super.onCodeSent(s, forceResendingToken);
+            //Log.d("code",codeSent);
+            codeSent = s;
+        }
+    };
+
+}
--- app/src/main/java/com/dev/pankaj/gcreationv1/MainActivity.java
+++ app/src/main/java/com/dev/pankaj/gcreationv1/MainActivity.java
@@ -2,7 +2,12 @@ package com.dev.pankaj.gcreationv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser==null)
+        {
+            Toast.makeText(this,"Please signin",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this,AuthenticateUser.class));
+        }
+        else
+        {
+            Toast.makeText(this,"Welcome",Toast.LENGTH_LONG).show();
+        }
     }
 }
